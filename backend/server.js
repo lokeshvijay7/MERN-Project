bcf95c7a5b1da234612000c8bed0f7c9e0f43698
lokeshvijay7 <lokeshlokesh93661@gmail.com>
2025-05-11 22:00:46 +0530

    server

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,5 @@
 import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';  
-
-dotenv.config();
-
-
 const app = express();
-
-app.get('/', (req ,res) =>{
-  res.send("Rich Broo...")
-})
-
-
 app.listen(3000, ()=>{
   console.log('server start on port 3000 at http://localhost:3000');
 
