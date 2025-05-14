90430232345f1df4fb59ef0f5e8cba7dcf9575e2
lokeshvijay7 <lokeshlokesh93661@gmail.com>
2025-05-14 14:50:48 +0530

    Bug Fix

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,22 @@
 import express from 'express';
 // import dotenv from 'dotenv';
 import connect_db from './config/db.js';
-import product from './models/product.model.js';
+import product from './models/product.m.js';
 
 // dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+
+app.get('/api/products', async (req, res) => {
+  try {
+    const products = await product.find({});
+    return res.status(200).json(products);
+  } catch (err) {
+    return res.status(500).json({ message: 'Server error' });
+  }
+});
 
 app.post('/api/products', async (req, res) => {
   const product_data = req.body;
@@ -25,23 +34,12 @@ app.post('/api/products', async (req, res) => {
   }
 });
 
-// connect_db().then(() => {
-//   console.log('Mongo db connected');
-//   app.listen(3000, () => {
-//     console.log('server start on port 3000 at http://localhost:3000');
-//   });
-// }).catch((err) => {
-//   console.error('Failed to connect to MongoDB', err);
-// });
-
 try {
   connect_db();
   app.listen(3000, () => {
     console.log('server start on port 3000 at http://localhost:3000');
   });
-
 }
 catch(err){
   console.log('Mongo db connection error:', err);
-
 }
